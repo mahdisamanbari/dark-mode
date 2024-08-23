b77a60b8c7a2f8be087c5d751c156011f861f4e3
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-08-23 13:38:19 +0330

    content jsx & style added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,31 @@ const App = () => {
         </div>
       </header>
       <div className='content'>
+        <div className='container'>
+          <h2>This is a light-mode project and now the page is light</h2>
+          <p>color of page is light</p>
+
+        </div>
+        <div className='container'>
+          <div className='cart'>
+            <div className='cart-item'>
+              <img src='image/2.jpg' />
+              <h3>cart 1</h3>
+              <p>this is cart 1</p>
+            </div>
+            <div className='cart-item'>
+              <img src='image/6.jpg' />
+              <h3>cart 2</h3>
+              <p>this is cart 2</p>
+            </div>
+            <div className='cart-item'>
+              <img src='image/7.jpg' />
+              <h3>cart 3</h3>
+              <p>this is cart 3</p>
+            </div>
+          </div>
+          
+        </div>
 
       </div>
       
