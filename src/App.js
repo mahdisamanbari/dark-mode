132d909db1cb737a4381bbf32075ad5eb786a833
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-08-23 12:48:55 +0330

    header jsx & style added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
+      <header className='header'>
+        <div className='container'>
+          <div className='nav'>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact us</li>
+            </ul>
+            <label className='switch'>
+              <input type='checkbox' />
+              <span className='slider round'></span>
+            </label>
+
+          </div>
+
+        </div>
+      </header>
+      <div className='content'>
+
+      </div>
       
     </div>
   );
